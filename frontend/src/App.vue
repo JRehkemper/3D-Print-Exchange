774891db846d265cf8e069fcb8ee5f58fcc54751
JadBlackstone <jannik.rehkemper@gmail.com>
2022-03-30 19:12:34 +0200

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main color="background">
+      <router-view/>
+      <v-btn>test</v-btn>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/TheHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style>
+/*:root {
+  --primary-color: #011f4b
+}*/
+
+</style>
---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        background: '#999'
+      }
+    }
+  } 
+}
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
