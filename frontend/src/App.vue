b115f3831da653596a148d039a0bd22e337a3c74
JadBlackstone <jannik.rehkemper@gmail.com>
2022-03-31 21:31:00 +0200

    ...

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,20 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar clipped-left rounded app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>3D PrintExchange</v-toolbar-title>
-    </v-app-bar>
-    
-    <v-navigation-drawer v-model="drawer" permament clipped app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    
-
-    <v-main class="pa-6 mt-10">
-      <v-container class="fluid">
-        <router-view>
-        </router-view>
-      </v-container>
-    </v-main>
+    <NavSideBar />
+    <v-content class="pa-6 mt-10">
+      <router-view> </router-view>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import NavSideBar from "./components/NavSideBar.vue";
 export default {
-  data() {
-    return {
-      drawer: null,
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
-      ]     
-    }
+  components: {
+    NavSideBar,
   },
-}
+  data() {
+    return {};
+  },
+};
 </script>
---
+++ frontend/src/components/NavSideBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>3D PrintExchange</v-toolbar-title>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" clipped app>
+            <v-list-item>
+                <v-list-item-content>
+                <v-list-item-title class="text-h6"> Application </v-list-item-title>
+                <v-list-item-subtitle> subtext </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            drawer: null,
+            items: [
+                { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+                { title: 'Photos', icon: 'mdi-image' },
+                { title: 'About', icon: 'mdi-help-box' },
+            ]
+        }
+    }
+}
+</script>
