5a1a0626aadd43816f5e6739d1b9b6a3f7be1ff1
JadBlackstone <jannik.rehkemper.de>
2022-04-01 16:27:43 +0200

    added BrowseView

---
+++ frontend/src/components/ListCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="pa-3">
+    <v-card-title>{{ item.title }}</v-card-title>
+    <v-img
+      src="https://picsum.photos/200/300"
+      max-width="200"
+      max-height="200"
+    ></v-img>
+    <v-card-text>
+      {{ item.description }}
+    </v-card-text>
+    <v-card-action>
+      <v-btn class="ma-3">Open</v-btn>
+      <v-btn class="mx-2"  dark  color="red">
+        <v-icon dark left> mdi-heart </v-icon>{{ item.likes }}
+      </v-btn>
+    </v-card-action>
+  </v-card>
+</template>
+<script>
+export default {
+    props: [
+        'item'
+    ]
+}
+</script>
---
+++ frontend/src/views/BrowseView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Browseview</h1>
+        <div v-for="item in items" :key="item.id" class="ma-6">
+            <ListCard :item="item" />
+        </div>
+    </div>
+</template>
+<script>
+import ListCard from '../components/ListCard.vue'
+export default {
+    components: {
+        ListCard,
+    },
+    data() {
+        return {
+            items: [
+                {id: 1, title: "Test1", description: "Hallo ich bin Test1", likes: 5},
+                {id: 2, title: "Test2", description: "Hi I'm Test2", likes: 33462},
+            ],
+        }
+    }
+    
+}
+</script>
